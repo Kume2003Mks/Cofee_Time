751e9cff2c3f4551bba1154dd4addd19ad701599
Ittirad rumkrathok <mammot2546@gmail.com>
2023-06-19 23:24:23 +0700

    Pull Markers from Firestore

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,138 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, SafeAreaView } from 'react-native';
 import MapView, { Marker, MapToolbar } from 'react-native-maps';
 import SearchBar from '../component/SearchBar';
 import { GlobalStyles } from '../styles/GlobalStyles';
+import { collection, getDocs } from 'firebase/firestore/lite';
+import { DB } from '../AppConfig/firebase';
+
 
 const MapScreen = () => {
-  const markers = [
-    {
-      latitude: 14.884245,
-      longitude: 102.026255,
-      title: 'Class cafe ประตู 1',
-    },
-    {
-      latitude: 14.884385,
-      longitude: 102.026772,
-      title: 'Coffee and cream',
-    },
-    {
-      latitude: 14.884245,
-      longitude: 102.026255,
-      title: 'Class cafe ประตู 1',
-    },
-    {
-      latitude: 14.904933,
-      longitude: 102.010849,
-      title: 'More Milk',
-    },
-    {
-      latitude: 14.881884,
-      longitude: 102.015055,
-      title: 'Faraday Coffee',
-    },
-    {
-      latitude: 14.884385,
-      longitude: 102.026772,
-      title: 'Coffee and cream',
-    },
-    {
-      latitude: 14.922146,
-      longitude: 101.993077,
-      title: 'Polar Polar Cafeสาขา1',
-    },
-      {
-      latitude: 14.876306,
-      longitude: 102.043333,
-      title: 'Takeoff Cafe By Otaru @Airplane Park KoratBy Otaru',
-    },
-      {
-      latitude: 14.876379,
-      longitude: 102.033522,
-      title: 'นมหนมFOODมทส.ประตู1',
-    },
-      {
-      latitude: 14.907155,
-      longitude:102.011275,
-      title: 'กาแฟเถื่อนละมุน ',
-    },
-      {
-      latitude: 14.907810,
-      longitude: 102.004021,
-      title: 'Ananya Cafe',
-    },
-      {
-      latitude: 14.902751,
-      longitude: 102.009679,
-      title: 'จุด 3 จุดมทส',
-    },
-      {
-      latitude: 14.885112,
-      longitude: 102.024944,
-      title: 'ลัลลา',
-    },
-        {
-      latitude: 14.909277,
-      longitude: 102.014432,
-      title: 'ลานหอมลานนม ',
-    },
-        {
-      latitude: 14.864268,
-      longitude: 102.036065,
-      title: 'Alert Coffee',
-    },
-        {
-      latitude: 14.885112,
-      longitude: 102.024944,
-      title: 'ลัลลา',
-    },
-        {
-      latitude: 14.878558,
-      longitude: 102.030909,
-      title: 'ปั๊มนม Pอุ้ม',
-    },
-        {
-      latitude: 14.904888,
-      longitude: 102.011198,
-      title: 'HOME cafe',
-    },
-         {
-      latitude: 14.883680,
-      longitude: 102.024923,
-      title: 'Cafe Amazon',
-    },
-         {
-      latitude: 14.899739,
-      longitude: 102.051973,
-      title: 'Sata Cafe',
-    },
-         {
-      latitude: 14.874749,
-      longitude: 102.015339,
-      title: 'POLAR POLAR Campus Cafe',
-    },
-         {
-      latitude: 14.904731,
-      longitude: 102.010880,
-      title: 'The Bloom Cafe',
-    },
-         {
-      latitude: 14.898599,
-      longitude: 102.049968,
-      title: 'Little Hut Cafe',
-    },
-         {
-      latitude: 14.901417,
-      longitude: 102.069500,
-      title: 'Saii Cafe',
-    },
-         {
-      latitude: 14.903162,
-      longitude: 102.056476,
-      title: 'Ob-Oon Bakery&Coffee ',
-    },
-    // Add more markers as needed
-  ];
+  const [mapList, setMenuList] = useState([]);
+
+ 
+  useEffect(() => {
+    const getMenu = async () => {
+      try {
+        const mapColRef = collection(DB, 'Markers');
+        const mapSnapshot = await getDocs(mapColRef);
+        const mapListData = mapSnapshot.docs.map(doc => ({
+          id: doc.id,
+          latitude: doc.data().location.latitude,
+          longitude: doc.data().location.longitude,
+          title: doc.data().title
+        }));
+        setMenuList(mapListData);
+      } catch (error) {
+        console.error('Error fetching menu:', error);
+      }
+    };
+  
+    getMenu();
+  }, []);
 
   const initialRegion = {
     latitude: 14.882018,   // Latitude of Suranaree University Of Technology
@@ -148,13 +45,20 @@ const MapScreen = () => {
       </View>
       <View style={styles.container}>
       <MapView style={styles.map} initialRegion={initialRegion}>
-          {markers.map((marker, index) => (
-            <Marker
-              key={index}
-              coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
-              title={marker.title}
-            />
-          ))}
+          {mapList.map((item) => {
+  const { id, latitude, longitude, title } = item;
+  if (latitude && longitude) {
+    return (
+      <Marker
+        key={id}
+        coordinate={{ latitude, longitude }}
+        title={title}
+      />
+    );
+  }
+  return null; // Skip rendering the marker if latitude or longitude is missing
+})}
+
         </MapView>
       </View>
     </View>
